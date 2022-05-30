2bedef8c945df3972730a796c3888e14e3221749
Salman <salmanpp05@gmail.com>
2022-05-30 15:59:08 +0530

    to heroku

--- ecertificate/settings.py
+++ ecertificate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY =config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecertificate-creator.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
